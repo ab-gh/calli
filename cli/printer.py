79efa77660d6224949b4db95d6eb19db1fa69e8d
aejb <thomas.bass2001@gmail.com>
2020-07-01 22:04:44 +0100

    changes API to be more RESTy, begins timezone handling

--- cli/printer.py
+++ cli/printer.py
@@ -1,10 +1,15 @@
 import json
 import inflect
+import datetime as dt
+from dateutil import tz
 
 def pretty(content):
     p = inflect.engine()
+    print(content)
     content = json.loads(content)
     print(f"{p.no('event', len(content))} today")
     for row in content:
         if row["type"] == "E":
-            print(f"{row['eventhex'].lower()} -> {row['start']} {row['name']}")
+            utc = dt.datetime.utcfromtimestamp(int(row['start']))
+            print(local)
+            print(f"{row['eventhex'].lower()} -> {local.strftime('%I:%M%p')} {row['name']}")
