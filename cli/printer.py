1f6b85c7e57d98c280a7a742f31814e8505cc113
aejb <thomas.bass2001@gmail.com>
2020-07-02 12:57:00 +0100

    adds delete command

--- cli/printer.py
+++ cli/printer.py
@@ -5,11 +5,15 @@ from dateutil import tz
 
 def pretty(content):
     p = inflect.engine()
-    print(content)
     content = json.loads(content)
     print(f"{p.no('event', len(content))} today")
     for row in content:
         if row["type"] == "E":
-            utc = dt.datetime.utcfromtimestamp(int(row['start']))
-            print(local)
-            print(f"{row['eventhex'].lower()} -> {local.strftime('%I:%M%p')} {row['name']}")
+            pointy = "->"
+        elif row["type"] == "R":
+            pointy = "-*"
+        else:
+            pointy = "-?"
+        utc = dt.datetime.utcfromtimestamp(int(row['start']))
+        local = utc.replace(tzinfo=dt.timezone.utc).astimezone(tz=None)
+        print(f"{row['eventhex'].lower()} {pointy} {local.strftime('%I:%M%p')} {row['name']}")
